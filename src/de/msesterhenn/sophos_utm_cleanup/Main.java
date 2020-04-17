321bd94eb9f9146e1a53a9dc897c47623b237a80
Maximilian Sesterhenn <kontakt@msesterhenn.de>
2020-04-17 21:17:47 +0200

    update

--- src/de/msesterhenn/sophos_utm_cleanup/Main.java
+++ src/de/msesterhenn/sophos_utm_cleanup/Main.java
@@ -1,6 +1,7 @@
 package de.msesterhenn.sophos_utm_cleanup;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.KeyManagementException;
@@ -14,17 +15,13 @@ import java.util.Map.Entry;
 
 import org.apache.commons.lang3.SerializationUtils;
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
 
@@ -33,152 +30,234 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 public class Main {
-	
 
-    // one instance, reuse
-    static CloseableHttpClient httpClient = null;
-    
-    static BufferedReader reader =
-            new BufferedReader(new InputStreamReader(System.in));
-    
-    static Gson gson = new Gson();
-    static JsonParser jsonparser = new JsonParser();
+	// one instance, reuse
+	static CloseableHttpClient httpClient = null;
+
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+	static Gson gson = new Gson();
+	static JsonParser jsonparser = new JsonParser();
 
 	public static void main(String[] args) {
-		
-		try 
-		{
-			httpClient = HttpClients
-			        .custom()
-			        .setSSLContext(new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build())
-			        .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-			        .build();
-		} 
-		catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e1) 
-		{
+
+		ArrayList<String> itemGroupsToCheck = new ArrayList<String>();
+
+		itemGroupsToCheck.add("availability_group");
+		itemGroupsToCheck.add("dns_group");
+		itemGroupsToCheck.add("dns_host");
+		itemGroupsToCheck.add("group");
+		itemGroupsToCheck.add("host");
+		itemGroupsToCheck.add("network");
+		itemGroupsToCheck.add("range");
+
+		try {
+			httpClient = HttpClients.custom()
+					.setSSLContext(new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build())
+					.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE).build();
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		String apibase = "";
 		String apikey = "";
 
-		try 
-		{
+		try {
 			System.out.println("Enter UTM API Base (e.g. https://192.168.30.10:4444/api/) :");
-			
+
 			apibase = reader.readLine();
 
 			System.out.println("Enter UTM API Key:");
-		
+
 			apikey = reader.readLine();
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		HashMap<String, String> headers = new HashMap<String, String>();
-		headers.put("Accept", "application/json");
-		headers.put("Authorization", "Basic " + Base64.getEncoder().encodeToString(("token:" + apikey).getBytes()));
-		
-		
-		ArrayList<String> networkItemsToDelete = new ArrayList<String>();
-        
-		try 
-		{
-			String networkList = sendGet(apibase + "/objects/network/network/", headers);
-			JsonElement parsednetworkList = jsonparser.parse(networkList);
-			
-			for(JsonElement entry: parsednetworkList.getAsJsonArray()) {
-				String networkUsedBy = sendGet(apibase + "/objects/network/network/" + entry.getAsJsonObject().get("_ref").getAsString() + "/usedby", headers);
-				JsonElement parsedNetworkUsedBy = jsonparser.parse(networkUsedBy);
-				
-				if (parsedNetworkUsedBy.getAsJsonObject().get("objects").getAsJsonArray().size() == 0 && parsedNetworkUsedBy.getAsJsonObject().get("nodes").getAsJsonArray().size() == 0) {
-					if (entry.getAsJsonObject().get("_locked").getAsString().isEmpty()) 
-					{
-						System.out.println("Empty and not locked: "  + entry.getAsJsonObject().get("name"));
-						
-						networkItemsToDelete.add(entry.getAsJsonObject().get("_ref").getAsString());
+
+		try {
+			FileWriter csvWriter = new FileWriter("deleted_items.csv");
+
+			csvWriter.append("ItemGroup;ItemRef;ItemName;ItemValue\n");
+
+			HashMap<String, List<String>> ItemsToDelete = new HashMap<String, List<String>>();
+
+			HashMap<String, String> headers = new HashMap<String, String>();
+			headers.put("Accept", "application/json");
+			headers.put("Authorization", "Basic " + Base64.getEncoder().encodeToString(("token:" + apikey).getBytes()));
+
+			for (String itemGroup : itemGroupsToCheck) {
+
+				ItemsToDelete.putIfAbsent(itemGroup, new ArrayList<String>());
+
+				try {
+
+					int itemcount = 0;
+
+					String itemList = sendGet(apibase + "/objects/network/" + itemGroup + "/", headers);
+					JsonElement parsedItemList = jsonparser.parse(itemList);
+
+					for (JsonElement entry : parsedItemList.getAsJsonArray()) {
+						String itemUsedBy = sendGet(apibase + "/objects/network/" + itemGroup + "/"
+								+ entry.getAsJsonObject().get("_ref").getAsString() + "/usedby", headers);
+						JsonElement parsedItemUsedBy = jsonparser.parse(itemUsedBy);
+
+						if (parsedItemUsedBy.getAsJsonObject().get("objects").getAsJsonArray().size() == 0
+								&& parsedItemUsedBy.getAsJsonObject().get("nodes").getAsJsonArray().size() == 0) {
+							if (entry.getAsJsonObject().get("_locked").getAsString().isEmpty()) {
+								System.out.println("Empty and not locked: " + entry.getAsJsonObject().get("name"));
+
+								ItemsToDelete.get(itemGroup).add(entry.getAsJsonObject().get("_ref").getAsString());
+								itemcount++;
+								
+							}
+						}
 					}
+
+					System.out
+							.println("Detected " + itemcount + " unused " + itemGroup + " items to delete. Continue?");
+					reader.readLine();
+					System.out.println("Are you really sure?");
+					reader.readLine();
+
+					for (JsonElement entry : parsedItemList.getAsJsonArray()) 
+					{
+						if (ItemsToDelete.get(itemGroup).contains(entry.getAsJsonObject().get("_ref").getAsString())) {
+							
+							String itemValue = "";
+							
+							if (itemGroup.equals("availability_group")) 
+							{
+								StringBuilder sb = new StringBuilder();
+								for (JsonElement e : entry.getAsJsonObject().get("members").getAsJsonArray())
+								{
+									sb.append(e.getAsString());
+									sb.append(",");
+								}
+								
+								itemValue = sb.toString();
+								
+							}
+							else if (itemGroup.equals("dns_group"))
+							{
+								itemValue = entry.getAsJsonObject().get("hostname").getAsString();
+							}
+							else if (itemGroup.equals("dns_host"))
+							{
+								itemValue = entry.getAsJsonObject().get("hostname").getAsString();
+							}
+							else if (itemGroup.equals("group"))
+							{
+								StringBuilder sb = new StringBuilder();
+								for (JsonElement e : entry.getAsJsonObject().get("members").getAsJsonArray())
+								{
+									sb.append(e.getAsString());
+									sb.append(",");
+								}
+								
+								itemValue = sb.toString();
+							}
+							else if (itemGroup.equals("host"))
+							{
+								itemValue = entry.getAsJsonObject().get("address").getAsString();
+							}
+							else if (itemGroup.equals("network"))
+							{
+								StringBuilder sb = new StringBuilder();
+								sb.append(entry.getAsJsonObject().get("address").getAsString());
+								sb.append("/" + entry.getAsJsonObject().get("netmask").getAsInt());
+								
+								itemValue = sb.toString();
+							}
+							else if (itemGroup.equals("range"))
+							{
+								StringBuilder sb = new StringBuilder();
+								sb.append(entry.getAsJsonObject().get("from").getAsString());
+								sb.append("-");
+								sb.append(entry.getAsJsonObject().get("to").getAsString());
+								
+								itemValue = sb.toString();
+							}
+							
+							csvWriter.append(itemGroup + ";" + entry.getAsJsonObject().get("_ref").getAsString()  + ";" + entry.getAsJsonObject().get("name") + ";" + itemValue + "\n");
+							
+							
+						}
+					}
+					
+					csvWriter.flush();
+					
+					HashMap<String, String> headersDelete = SerializationUtils.clone(headers);
+					headersDelete.put("X-Restd-Err-Ack", "all");
+
+					for (String s : ItemsToDelete.get(itemGroup)) {
+						sendDelete(apibase + "/objects/network/" + itemGroup + "/" + s, headersDelete);
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
+
 			}
 			
-			System.out.println("Detected " + networkItemsToDelete.size() + " unused items to delete. Continue?");
-			reader.readLine();
-			System.out.println("Are you really sure?");
-			reader.readLine();
-			
-			HashMap<String, String> headersDelete = SerializationUtils.clone(headers);
-			headersDelete.put("X-Restd-Err-Ack", "all");
-			
-			
-			for (String s : networkItemsToDelete) 
-			{
-				sendDelete(apibase + "/objects/network/network/" + s, headersDelete);
-			}
-			
-			System.out.println("Completed.");
-			
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace();
+			csvWriter.close();
+
+		} catch (IOException e2) {
+			e2.printStackTrace();
 		}
 		
+		System.out.println("Completed.");
+
 	}
-	
-    static void close() throws IOException {
-        httpClient.close();
-    }
 
-    static String sendGet(String url, HashMap<String, String> headers) throws Exception {
+	static void close() throws IOException {
+		httpClient.close();
+	}
 
-        HttpGet request = new HttpGet(url);
+	static String sendGet(String url, HashMap<String, String> headers) throws Exception {
 
-        // add request headers
-        for(Entry<String, String> entry : headers.entrySet())
-        {
-        	request.addHeader(entry.getKey(), entry.getValue());
-        }
+		HttpGet request = new HttpGet(url);
 
-        try (CloseableHttpResponse response = httpClient.execute(request)) {
+		// add request headers
+		for (Entry<String, String> entry : headers.entrySet()) {
+			request.addHeader(entry.getKey(), entry.getValue());
+		}
 
-            HttpEntity entity = response.getEntity();
+		try (CloseableHttpResponse response = httpClient.execute(request)) {
 
-            if (entity != null) {
-                // return it as a String
-            	return EntityUtils.toString(entity);
-            }
-            
-            return null;
+			HttpEntity entity = response.getEntity();
 
-        }
+			if (entity != null) {
+				// return it as a String
+				return EntityUtils.toString(entity);
+			}
 
-    }
+			return null;
 
-    static String sendDelete(String url, HashMap<String, String> headers) throws Exception {
+		}
 
-    	HttpDelete delete = new HttpDelete(url);
+	}
 
-        // add request headers
-        for(Entry<String, String> entry : headers.entrySet())
-        {
-        	delete.addHeader(entry.getKey(), entry.getValue());
-        }
+	static String sendDelete(String url, HashMap<String, String> headers) throws Exception {
 
-        try (CloseableHttpResponse response = httpClient.execute(delete)) {
+		HttpDelete delete = new HttpDelete(url);
 
-            HttpEntity entity = response.getEntity();
+		// add request headers
+		for (Entry<String, String> entry : headers.entrySet()) {
+			delete.addHeader(entry.getKey(), entry.getValue());
+		}
 
-            if (entity != null) {
-                // return it as a String
-            	return EntityUtils.toString(entity);
-            }
-            
-            return null;
+		try (CloseableHttpResponse response = httpClient.execute(delete)) {
 
-        }
-    }
+			HttpEntity entity = response.getEntity();
+
+			if (entity != null) {
+				// return it as a String
+				return EntityUtils.toString(entity);
+			}
+
+			return null;
+
+		}
+	}
 
 }
